ec535e0d1ff59739f2de7cc34f6566d0b2a5843b
FawzyFathi01 <fawzyfathi6@gmail.com>
2024-07-13 18:36:35 +0300

    Describe the changes you made for 2

--- Level_1/01_Prefix_Frequeny/02_CodePlay/C++/ContestCL/Contest.cpp
+++ Level_1/01_Prefix_Frequeny/02_CodePlay/C++/ContestCL/Contest.cpp
@@ -22,10 +22,10 @@ void Start_Contest () {
 // Prefix_Sum
 void Solve_1 () {
     int size;
-    cin>>size;
+    cin >> size;
     int arr[size];
-    for (auto &i :arr){
-        cin>>i;
+    for (auto &i: arr) {
+        cin >> i;
     }
 
     //1- Calculate PreFix_Sum
@@ -35,78 +35,81 @@ void Solve_1 () {
     // How ==>  prefix[i]= prefix[i-1] + array[i]
     int prefix[size];
     prefix[0] = arr[0];
-    for(int i=1;i<size;i++){
-        prefix[i] = prefix[i-1] + arr[i];
+    for (int i = 1; i < size; i++) {
+        prefix[i] = prefix[i - 1] + arr[i];
     }
-    cout<<"Prefix Array : - "<<endl;
-    for(auto i:prefix){
-        cout<<i<<' ';
+    cout << "Prefix Array : - " << endl;
+    for (auto i: prefix) {
+        cout << i << ' ';
     }
 
     //2- Sum Of Range
-    cout<<endl;
-    int q,a,b,res;
-    cin>>q;
-    while (q--){
-        cin>>a>>b;
-      //  if (a==0)res = prefix[b];
-      //  else res = prefix[b] - prefix[a-1];
-        (a==0)?res = prefix[b]:res = prefix[b] - prefix[a-1];
-        cout<<"Sum from "<<a<<" to "<<b<<" : "<<res<<endl;
+    cout << endl;
+    int q, a, b, res;
+    cin >> q;
+    while (q--) {
+        cin >> a >> b;
+        //  if (a==0)res = prefix[b];
+        //  else res = prefix[b] - prefix[a-1];
+        (a == 0) ? res = prefix[b] : res = prefix[b] - prefix[a - 1];
+        cout << "Sum from " << a << " to " << b << " : " << res << endl;
     }
 }
-void Solve_2(){
+
+void Solve_2() {
     int size;
-    cin>>size;
+    cin >> size;
     int arr[size];
-    for (auto &i :arr){
-        cin>>i;
+    for (auto &i: arr) {
+        cin >> i;
     }
     // Appear NumBer
     // Input Number / Range a , b
     // Prefix for Frequency Number 0 -> size-1
     // Answer Quire Rang
 
-    int t,num,q,a,b,res;
-    cin>>t;
-    while (t--){
-        cin>>num>>q;
+    int t, num, q, a, b, res;
+    cin >> t;
+    while (t--) {
+        cin >> num >> q;
         // Calculate Frequency For Num
-        int freSum [size]; freSum[0] = 0 + (arr[0] == num);
-        for (int i = 1;i<size;i++){
-            freSum[i] = freSum[i-1] + (arr[i] == num);
+        int freSum[size];
+        freSum[0] = 0 + (arr[0] == num);
+        for (int i = 1; i < size; i++) {
+            freSum[i] = freSum[i - 1] + (arr[i] == num);
         }
         // Quire Range
-        for (auto i:freSum){
-            cout<<i<<" ";
+        for (auto i: freSum) {
+            cout << i << " ";
         }
-        cout<<endl;
-        while (q--){
-            cin>>a>>b;
+        cout << endl;
+        while (q--) {
+            cin >> a >> b;
             //if (freSum[a] == 0) res = freSum[b];
             //else res = freSum[b] - freSum[a];
-            (freSum[a] == 0)?res = freSum[b] : res = freSum[b] - freSum[a];
-            cout<<"Count of Appear Num ( " <<num <<" ) From "<<a<<" to "<<b <<" : "<< res<<endl;
+            (freSum[a] == 0) ? res = freSum[b] : res = freSum[b] - freSum[a];
+            cout << "Count of Appear Num ( " << num << " ) From " << a << " to " << b << " : " << res << endl;
         }
 
     }
-
 }
-void Solve_3(){
+
+void Solve_3() {
     // Task Other
     // Arr ==> a , b / Min ( Sum(a) - Sum(b) )
 }
 
+
 // Partial_Sum
-void Solve_4(){
+void Solve_4() {
 
     // Input Array
     int size;
-    cin>>size;
-    int arr[size+1];
+    cin >> size;
+    int arr[size + 1];
 
-    for (int i=1;i<=size;i++){
-        cin>>arr[i];
+    for (int i = 1; i <= size; i++) {
+        cin >> arr[i];
     }
 
     // Update Array  for Range
@@ -114,18 +117,19 @@ void Solve_4(){
     // a,b,k
 
     arr[0] = 0;
-    int q,a,b,k; cin>>q;
-    while(q--){
-        cin>>a>>b>>k;
-        arr[a]+=k,arr[b+1]-=k;
+    int q, a, b, k;
+    cin >> q;
+    while (q--) {
+        cin >> a >> b >> k;
+        arr[a] += k, arr[b + 1] -= k;
     }
-    for (int i =1;i<=size;i++){
-        arr[i] +=arr[i-1];
+    for (int i = 1; i <= size; i++) {
+        arr[i] += arr[i - 1];
     }
-    for (int i = 1;i<=size;i++){
-        cout<<arr[i]<<" ";
+    for (int i = 1; i <= size; i++) {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
 
